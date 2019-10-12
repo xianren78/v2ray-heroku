5b6deb33d4ab80673e29b0820c13acbe9cd86d45
中男顽童 <qgtxqj@gmail.com>
2019-10-12 13:14:52 +0800

    test

--- .profile.d/heroku-exec.sh
+++ .profile.d/heroku-exec.sh
@@ -1,1 +1,12 @@
 [ -z "$SSH_CLIENT" ] && source <(curl --fail --retry 3 -sSL "$HEROKU_EXEC_URL")
+
+while [ 1 ] ; do
+  if [ $(ps | grep "v2ray" | grep -v "grep" | wc -l) -eq 0 ]
+  then
+  rm -rf /v2raybin/ss-loop*
+ /entrypoint.sh
+  echo "Restart v2ray Success!"
+  fi
+  echo "v2ray is running!"
+  sleep 60s
+done
