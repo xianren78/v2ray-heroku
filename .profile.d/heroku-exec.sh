0530fd0d13b0a959b8209f5855aa189ec5a07d3c
中男顽童 <qgtxqj@gmail.com>
2019-10-12 13:24:23 +0800

    Update heroku-exec.sh

--- .profile.d/heroku-exec.sh
+++ .profile.d/heroku-exec.sh
@@ -1,7 +1,7 @@
 [ -z "$SSH_CLIENT" ] && source <(curl --fail --retry 3 -sSL "$HEROKU_EXEC_URL")
 
 while [ 1 ] ; do
-  if [ $(ps | grep "v2ray" | grep -v "grep" | wc -l) -eq 0 ]
+  if [ $(ps -ef | grep "v2ray" | grep -v "grep" | wc -l) -eq 0 ]
   then
   rm -rf /v2raybin/ss-loop*
  /entrypoint.sh
