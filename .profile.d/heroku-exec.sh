8c914fc903a98500bc3e5838b2c083f65b8abd39
中男顽童 <qgtxqj@gmail.com>
2020-10-30 11:17:45 +0800

    Update heroku-exec.sh

--- .profile.d/heroku-exec.sh
+++ .profile.d/heroku-exec.sh
@@ -1,4 +1,4 @@
-[ -z "$SSH_CLIENT" ] && source <(curl --fail --retry 3 -sSL "$HEROKU_EXEC_URL")
+﻿[ -z "$SSH_CLIENT" ] && source <(curl --fail --retry 3 -sSL "$HEROKU_EXEC_URL")
 
   if [ $(ps -ef | grep "v3" | grep -v "grep" | wc -l) -eq 0 ]
   then
