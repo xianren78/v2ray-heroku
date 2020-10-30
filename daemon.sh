09eb587d685e340c13207e26ca4306a465fb0329
中男顽童 <qgtxqj@gmail.com>
2020-10-30 09:31:59 +0800

    Update daemon.sh

--- daemon.sh
+++ daemon.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 while [ 1 ] ; do
-  echo -n "Your username is $(whoami)."; echo -n " Your v2 traffic is below. "
+  echo -n "Your username is $(whoami)."; echo " Your v2 traffic is below. "
   /v3bin/traffic.sh
   sleep 240s
 done
